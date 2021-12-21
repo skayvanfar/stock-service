e82fec4799368bd28721e6b0c4540e4d7ddfbd68
Saeed kayvanfar <kayvanfar.sj@gamil.com>
2021-12-21 21:11:37 +0330

    embedded h2 db is added.

--- src/main/java/ir/sk/stock/model/Stock.java
+++ src/main/java/ir/sk/stock/model/Stock.java
@@ -5,6 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 import java.time.Instant;
 
@@ -15,13 +20,26 @@ import java.time.Instant;
 @Slf4j
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "TBL_STOCK")
 public class Stock {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="ID")
     private Long id;
 
+    @NotNull
+    @Pattern(regexp = "^\\w+$", message = "{constraints.Pattern.alphanumeric.message}")
+    @Column(name = "NAME", unique = true)
     private String name;
 
+    @NotNull
+    @DecimalMin(value = "0", message = "{constraints.Digits.positive}")
+    @Digits(integer = 12, fraction = 2, message = "{constraints.Digits.decimal.message}")
+    @Column(name = "CURRENT_PRICE")
     private BigDecimal currentPrice;
 
+    @Column(name = "LAST_UPDATED")
     private Instant lastUpdate;
 }
