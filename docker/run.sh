a99b23006a8f2157a9f1ca3e69b33c23b6df18b1
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-12 19:06:03 +0100

    add $JAVA_OPTS to run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
-getPort() {
-    echo $1 | cut -d : -f 3 | xargs basename
-}
-
-echo "********************************************************"
-echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
-echo "********************************************************"
-#while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
-echo "******** Database Server has started "
 
 echo "********************************************************"
 echo "Starting Stock Server"
 echo "********************************************************"
-exec java -Dspring.profiles.active=$PROFILE -jar /usr/local/stockservice/app.jar
+exec java $JAVA_OPTS -Dspring.profiles.active=$PROFILE -jar /usr/local/stockservice/app.jar
