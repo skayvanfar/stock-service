a8774ae35f1b9d7c236733dcaeafb5b054b6971a
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2023-12-16 19:05:58 +0100

    fix StockServiceImplTest

--- src/test/java/ir/sk/stock/service/StockServiceImplTest.java
+++ src/test/java/ir/sk/stock/service/StockServiceImplTest.java
@@ -2,6 +2,7 @@ package ir.sk.stock.service;
 
 import ir.sk.stock.dto.StockDTO;
 import ir.sk.stock.mapper.StockMapper;
+import ir.sk.stock.mapper.StockMapperImpl;
 import ir.sk.stock.model.Stock;
 import ir.sk.stock.repository.StockRepository;
 import java.math.BigDecimal;
@@ -18,6 +19,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 public class StockServiceImplTest {
@@ -40,36 +42,45 @@ public class StockServiceImplTest {
             new Stock(1L, "BBT", BigDecimal.valueOf(37.55), first),
             new Stock(2L, "BTT", BigDecimal.valueOf(38.55), second));
 
-    Page<Stock> pagedStocks = new PageImpl<>(stocks);
-    Mockito.when(stockRepository.findAll(Pageable.ofSize(10))).thenReturn(pagedStocks);
+    stockMapper = new StockMapperImpl();
 
     stockService = new StockServiceImpl(stockRepository, stockMapper);
   }
 
   @Test
   public void testGetAllStocksBySize() {
+    Page<Stock> pagedStocks = new PageImpl<>(stocks);
+    Mockito.when(stockRepository.findAll(Pageable.ofSize(10))).thenReturn(pagedStocks);
+
     int expectedSize = 2;
-    Page<StockDTO> Stocks = stockService.findAll(Pageable.ofSize(10));
-    Assertions.assertEquals(expectedSize, Stocks.getTotalElements());
+    Page<StockDTO> result = stockService.findAll(Pageable.ofSize(10));
+    List<StockDTO> expected =
+        stocks.stream().map(stock -> stockMapper.stockToStockDTO(stock)).toList();
+    Assertions.assertEquals(expectedSize, result.getTotalElements());
+    Assertions.assertEquals(expected, result.getContent());
   }
 
   @Test
   public void testGetAllStocksByContent() {
+    Page<Stock> pagedStocks = new PageImpl<>(stocks);
+    Mockito.when(stockRepository.findAll(Pageable.ofSize(10))).thenReturn(pagedStocks);
+
+    List<StockDTO> expected =
+        stocks.stream().map(stock -> stockMapper.stockToStockDTO(stock)).toList();
     Page<StockDTO> stocks = stockService.findAll(Pageable.ofSize(10));
-    Assertions.assertEquals(this.stocks, stocks.getContent());
+    Assertions.assertEquals(expected, stocks.getContent());
   }
 
   @Test
-  public void testSave() {
-    Stock newStock =
+  public void testCreate() {
+    Stock oldStock =
         new Stock(1L, "KKK", BigDecimal.valueOf(38.45), Instant.parse("1985-04-09T10:15:30.00Z"));
     StockDTO stockDTO =
         new StockDTO(
-            1L, "KKK", BigDecimal.valueOf(38.45), Instant.parse("1985-04-09T10:15:30.00Z"));
-    Mockito.when(stockRepository.save(newStock)).thenReturn(newStock);
-    String expectedName = "KKK";
+            1L, "KKK", BigDecimal.valueOf(45.08), Instant.parse("1985-04-09T10:15:30.00Z"));
+    Mockito.when(stockRepository.save(any(Stock.class))).thenReturn(oldStock);
     StockDTO stock = stockService.create(stockDTO);
     Assertions.assertNotNull(stock);
-    Assertions.assertEquals(expectedName, stock.getName());
+    Assertions.assertEquals(oldStock.getCurrentPrice(), stock.getCurrentPrice());
   }
 }
